6591d4da19aa97e187a7727140afe0179412d243
DixitPansuriya <165665364+DixitPansuriya@users.noreply.github.com>
2024-05-04 11:51:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Header from './Component/Component/Header'
+import Herosection from './Component/Component/Herosection'
+import Buyonline from './Component/Component/Buyonline'
+import Other from './Component/Component/Other'
+import Best from './Component/Component/Best'
+import Mobiles from './Component/Component/Mobiles'
+import Login from './Component/Component/Login'
+import Rewiew from './Component/Component/Rewiew'
+
+import Footer from './Component/Component/Footer'
+
+
+
+
+
+
+
+export default function App() {
+  return (
+    <div>
+<Header/>
+<Herosection/>
+<Buyonline/>
+<Other/>
+<Best/>
+<Mobiles/>
+<Login/>
+<Rewiew/>
+
+<Footer/>
+
+    </div>
+  )
+}
